5c5a7ace2449615cf92551d9173ace10512b3fae
na2kera <nat3mitaro@gmail.com>
2024-06-09 17:12:24 +0900

    ユーザーデータの保存

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { use, useEffect } from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 const MyPage = () => {
   const { data: session, status } = useSession();
+
+  useEffect(() => {
+    const saveUser = async () => {
+      if (session?.user) {
+        await fetch("/api/saveUser", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name: session.user.name,
+            email: session.user.email,
+            image: session.user.image,
+          }),
+        });
+      }
+    };
+    saveUser();
+  }, [session]);
 
   if (!session) {
     return (
