3803452908dc8c81dba0b7662f447018e33ad5aa
na2kera <nat3mitaro@gmail.com>
2024-06-10 15:35:49 +0900

    タイムライン用のデータ取得

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,49 @@ import Auth from "./components/Auth";
 import EditIcon from "@mui/icons-material/Edit";
 import { Fab } from "@mui/material";
 import Link from "next/link";
+import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+
+type Post = {
+  email: string;
+  good: string;
+  keep: string;
+  problem: string;
+  action: string;
+  comment: string;
+  user: { email: string; name: string; image: string };
+};
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session, status } = useSession();
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getUserPosts = async () => {
+      const res = await fetch("/api/getUsersPosts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+      if (Array.isArray(data.data)) {
+        setPosts(data.data);
+      } else {
+        setPosts([]);
+      }
+    };
+    getUserPosts();
+  }, [session]);
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <Auth />
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+        <Auth />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -39,6 +55,16 @@ export default function Home() {
             <EditIcon />
           </Fab>
         </Link>
+      </div>
+      <div>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <h3>{post.good}</h3>
+            <p>{post.action}</p>
+
+            <br />
+          </div>
+        ))}
       </div>
     </main>
   );
