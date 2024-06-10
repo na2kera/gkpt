8360874650292b9d8f9a9a7bfae5e4a5be3bd3dd
na2kera <nat3mitaro@gmail.com>
2024-06-10 22:07:38 +0900

    フォームの修正

--- src/pages/createPost.tsx
+++ src/pages/createPost.tsx
@@ -1,7 +1,10 @@
-import { Box, Button, CardContent } from "@mui/material";
+import { Box, Button, CardContent, Fab } from "@mui/material";
 import { useSession } from "next-auth/react";
 import React, { useState } from "react";
 import SendIcon from "@mui/icons-material/Send";
+import router from "next/router";
+import Link from "next/link";
+import HomeIcon from "@mui/icons-material/Home";
 
 const CreatePost = () => {
   const { data: session, status } = useSession();
@@ -35,70 +38,78 @@ const CreatePost = () => {
         comment,
       }),
     });
+    router.push("/");
   };
   return (
-    <div className="max-w-xl mx-auto p-5">
-      <h1 className="text-2xl font-bold mb-5">今日の振り返りを入力</h1>
-      <div className="border border-gray-300 p-5 rounded">
-        <form onSubmit={handleSubmit}>
-          <div className="mb-5">
-            <label className="block mb-2">good</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setGood(e.target.value)
-              }
-            />
-          </div>
-          <div className="mb-5">
-            <label className="block mb-2">keep</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setKeep(e.target.value)
-              }
-            />
-          </div>
-          <div className="mb-5">
-            <label className="block mb-2">problem</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setProblem(e.target.value)
-              }
-            />
-          </div>
-          <div className="mb-5">
-            <label className="block mb-2">try</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setAction(e.target.value)
-              }
-            />
-          </div>
-          <div className="mb-5">
-            <label className="block mb-2">ひとこと</label>
-            <input
-              type="text"
-              className="w-full p-2 border border-gray-300 rounded"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setComment(e.target.value)
-              }
-            />
-          </div>
-          <Box display={"flex"} justifyContent={"center"}>
-            <Button type="submit" variant="contained" endIcon={<SendIcon />}>
-              Send
-            </Button>
-          </Box>
-        </form>
+    <>
+      <div className="max-w-xl mx-auto p-5">
+        <h1 className="text-2xl font-bold mb-5">今日の振り返りを入力</h1>
+        <div className="border border-gray-300 p-5 rounded">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-5">
+              <label className="block mb-2">good</label>
+              <textarea
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setGood(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2">keep</label>
+              <textarea
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setKeep(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2">problem</label>
+              <textarea
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setProblem(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2">try</label>
+              <textarea
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setAction(e.target.value)
+                }
+              />
+            </div>
+            <div className="mb-5">
+              <label className="block mb-2">ひとこと</label>
+              <textarea
+                required
+                className="w-full p-2 border border-gray-300 rounded"
+                onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                  setComment(e.target.value)
+                }
+              />
+            </div>
+            <Box display={"flex"} justifyContent={"center"}>
+              <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+                Send
+              </Button>
+            </Box>
+          </form>
+        </div>
       </div>
-    </div>
+      <Link className="fixed left-4 bottom-4" href="/">
+        <Fab color="secondary" aria-label="edit">
+          <HomeIcon />
+        </Fab>
+      </Link>
+    </>
   );
 };
 
