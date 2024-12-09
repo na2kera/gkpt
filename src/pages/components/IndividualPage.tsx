41935fe7d33b62f0457feb1494594ae04971e593
na2kera <nat3mitaro@gmail.com>
2024-12-09 15:10:45 +0900

    他人のページではアイコンが出ないように変更

--- src/pages/components/IndividualPage.tsx
+++ src/pages/components/IndividualPage.tsx
@@ -1,8 +1,9 @@
 import { Avatar, Box, Fab, Link } from "@mui/material";
-import { signIn, useSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import React from "react";
 import PostCard from "./PostCard";
 import HomeIcon from "@mui/icons-material/Home";
+import { useParams } from "next/navigation";
 
 type Props = {
   posts: Post[];
@@ -10,58 +11,53 @@ type Props = {
 
 const IndividualPage = ({ posts }: Props) => {
   const { data: session, status } = useSession();
+  const params = useParams();
+  const uuid = params.uuid;
 
-  if (!session) {
-    return (
-      <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-      </>
-    );
-  }
-
-  if (session) {
-    return (
-      <>
-        <Box
-          position={"absolute"}
-          display={"flex"}
-          flexDirection={"row"}
-          top={20}
-          right={12}
-        >
-          {session.user?.image && <Avatar src={session.user.image}></Avatar>}
-          <Box
-            px={2}
-            display={"flex"}
-            alignItems={"center"}
-            fontWeight={"bold"}
-          >
-            {session.user?.name}
-          </Box>
+  return (
+    <>
+      <Box
+        position={"absolute"}
+        display={"flex"}
+        flexDirection={"row"}
+        top={20}
+        right={12}
+      >
+        {session && session.user?.image && session.user?.id === uuid && (
+          <>
+            <Avatar src={session.user.image}></Avatar>
+            <Box
+              px={2}
+              display={"flex"}
+              alignItems={"center"}
+              fontWeight={"bold"}
+            >
+              {session?.user?.name}
+            </Box>
+          </>
+        )}
+      </Box>
+      <Box
+        display="flex"
+        minHeight="100vh"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="space-between"
+        p={6}
+      >
+        <Box>
+          {posts.map((post, index) => (
+            <PostCard post={post} key={index} />
+          ))}
         </Box>
-        <Box
-          display="flex"
-          minHeight="100vh"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="space-between"
-          p={6}
-        >
-          <Box>
-            {posts.map((post, index) => (
-              <PostCard post={post} key={index} />
-            ))}
-          </Box>
-        </Box>
-        <Link className="fixed left-4 bottom-4" href="/">
-          <Fab color="secondary" aria-label="home">
-            <HomeIcon />
-          </Fab>
-        </Link>
-      </>
-    );
-  }
+      </Box>
+      <Link className="fixed left-4 bottom-4" href="/">
+        <Fab color="secondary" aria-label="home">
+          <HomeIcon />
+        </Fab>
+      </Link>
+    </>
+  );
 };
 
 export default IndividualPage;
